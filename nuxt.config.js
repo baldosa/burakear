0c5cd2eabec743d62ba5d770c862314b138e5642
Ema <mast3er@gmail.com>
2020-12-27 17:25:02 -0300

    gh-pages base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/burakear/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
