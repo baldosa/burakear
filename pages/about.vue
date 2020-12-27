6dbff54145d11a941c48ce1951994a7184101b9d
Ema <mast3er@gmail.com>
2020-12-27 17:40:33 -0300

    Initial dist subtree commit

--- pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-row>
-    <v-col class="text-center">
-      Contador de puntos de BURAKO
-    </v-col>
-  </v-row>
-</template>
--- pages/rules.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-row>
-    <v-col class="text-center">
-      EVENTUALMENTE
-    </v-col>
-  </v-row>
-</template>
